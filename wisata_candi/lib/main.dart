2a8cda3af80d890ea1776387b7640bcd9b171346
art-trnty <164327639+art-trnty@users.noreply.github.com>
2024-11-04 12:36:55 +0700

    Pembaruan Files

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/sign_in_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,8 +19,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProfileScreen(),
+      // home: ProfileScreen(),
       //home: DetailScreen(candi: candiList[0]),
+      home: SignInScreen(),
     );
   }
 }
