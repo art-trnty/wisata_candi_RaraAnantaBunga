9a364cd167b2d9363e3fa687dee12fc96cc7efc8
art-trnty <164327639+art-trnty@users.noreply.github.com>
2024-11-09 09:43:31 +0700

    Update Files

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/search_screen.dart';
 import 'package:wisata_candi/screens/sign_in_screen.dart';
 import 'package:wisata_candi/screens/sing_up_screen.dart';
 
@@ -35,7 +36,8 @@ class MyApp extends StatelessWidget {
       // home: ProfileScreen(),
       // home: DetailScreen(candi: candiList[0]),
       // home: SignInScreen(),
-      home: SignUpScreen(),
+      home: SearchScreen(),
+      // home: SignUpScreen(),
     );
   }
 }
